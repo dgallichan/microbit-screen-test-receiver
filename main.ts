10baa8f2d0c70bd6129aabf5dd86e9a2670fa4a8
Daniel Gallichan <dgallichan@yahoo.com>
2022-05-20 13:22:09 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,26 @@
-radio.onReceivedString(function (receivedString) {
-    for (let index_rows = 0; index_rows <= 4; index_rows++) {
-        thisRowCalcVal = parseFloat(receivedString.substr(index_rows * 2, 2))
-        for (let index_cols = 0; index_cols <= 4; index_cols++) {
-            thisPixelValue = thisRowCalcVal % 2
-            thisRowCalcVal = Math.floor(thisRowCalcVal / 2)
-            if (thisPixelValue > 0) {
-                led.plot(4 - index_cols, index_rows)
-            } else {
-                led.unplot(4 - index_cols, index_rows)
+radio.onReceivedValue(function (name, value) {
+    if (name == "imAsNumber") {
+        thisPixelCalcVal = 0
+        for (let index_rows = 0; index_rows <= 4; index_rows++) {
+            for (let index_cols = 0; index_cols <= 4; index_cols++) {
+                thisPixelValue = thisPixelCalcVal % 2
+                thisPixelCalcVal = Math.floor(thisPixelCalcVal / 2)
+                if (thisPixelValue > 0) {
+                    led.plot(4 - index_cols, index_rows)
+                } else {
+                    led.unplot(4 - index_cols, index_rows)
+                }
             }
         }
     }
 })
 let thisPixelValue = 0
-let thisRowCalcVal = 0
+let thisPixelCalcVal = 0
 basic.showLeds(`
-    # # # # .
-    . . . # .
-    . # . # .
-    . # . # .
-    . # . # .
+    # # # . #
+    # . # . #
+    # # . . #
+    # . # . .
+    # . # . #
     `)
-radio.setGroup(1)
+radio.setGroup(99)
